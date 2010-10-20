3827c76a630764a487b3481cae46204bfe31ad66
Tim Besard <tim.besard@gmail.com>
2010-10-20 18:42:41 +0200

    Updated translation.

--- translations/nl_BE.ts
+++ translations/nl_BE.ts
@@ -4,62 +4,57 @@
 <context>
     <name>iRail::ConnectionRequestWidget</name>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="61"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="62"/>
         <source>Please fill in both locations.</source>
         <translation>Gelieve beide locaties in te vullen.</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="102"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="115"/>
         <source>Fetching list of stations</source>
         <translation>Ophalen stationslijst</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="103"/>
-        <source>Performing network query...</source>
-        <translation>Netwerkaanvraag verwerken...</translation>
-    </message>
-    <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="162"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="142"/>
         <source>New query</source>
         <translation>Nieuwe opzoeking</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="173"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="153"/>
         <source>Origin</source>
         <translation>Vertrekplaats</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="182"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="162"/>
         <source>Destination</source>
         <translation>Bestemming</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="196"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="176"/>
         <source>Departure</source>
         <translation>Vertrek</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="197"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="177"/>
         <source>Arrival</source>
         <translation>Aankomst</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="208"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="188"/>
         <source>Specify time and date</source>
         <translation>Specifiëer tijd en datum</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="246"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="226"/>
         <source>Clear</source>
         <translation>Wissen</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="247"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="227"/>
         <source>Set to now</source>
         <translation>Gebruik huidige tijd</translation>
     </message>
     <message>
-        <location filename="../ui/connectionrequestwidget.cpp" line="248"/>
+        <location filename="../ui/connectionrequestwidget.cpp" line="228"/>
         <source>Search</source>
         <translation>Zoeken</translation>
     </message>
@@ -67,9 +62,29 @@
 <context>
     <name>iRail::ConnectionResultWidget</name>
     <message>
-        <location filename="../ui/connectionresultwidget.cpp" line="34"/>
+        <location filename="../ui/connectionresultwidget.cpp" line="26"/>
+        <source>Fetching list of connections</source>
+        <translation>Ophalen verbindingen</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectionresultwidget.cpp" line="48"/>
         <source>Connections - %1 to %2</source>
         <translation>Verbindingen - %1 naar %2</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectionresultwidget.cpp" line="91"/>
+        <source>%1 (%2) to %3 (%4)</source>
+        <translation>%1 (%2) naar %3 (%4)</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectionresultwidget.cpp" line="100"/>
+        <source>%1 (%2) to %3 (%4), via %5 others</source>
+        <translation>%1 (%2) naar %3 (%4), via %5 andere</translation>
+    </message>
+    <message>
+        <location filename="../ui/connectionresultwidget.cpp" line="116"/>
+        <source>No connections found.</source>
+        <translation>Geen connecties gevonden.</translation>
     </message>
 </context>
 <context>
@@ -118,7 +133,7 @@
         <translation>Kies een station</translation>
     </message>
     <message>
-        <location filename="../ui/dialogs/stationchooser.cpp" line="44"/>
+        <location filename="../ui/dialogs/stationchooser.cpp" line="43"/>
         <source>Select</source>
         <translation>Voltooid</translation>
     </message>
