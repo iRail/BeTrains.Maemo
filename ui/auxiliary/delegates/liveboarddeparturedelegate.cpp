7a178188d9909552c3c0216e4a4f16dd4ac7964a
Tim Besard <tim.besard@gmail.com>
2010-11-30 20:28:40 +0100

    Liveboard delays.

--- ui/auxiliary/delegates/liveboarddeparturedelegate.cpp
+++ ui/auxiliary/delegates/liveboarddeparturedelegate.cpp
@@ -14,35 +14,68 @@ using namespace iRail;
 
 void LiveboardDepartureDelegate::paint(QPainter *iPainter, const QStyleOptionViewItem &iOption, const QModelIndex &iIndex) const
 {
-    QStyledItemDelegate::paint(iPainter, iOption, iIndex);
+    //
+    // Fetch data
+    //
 
     if (! qVariantCanConvert<Liveboard::Departure>(iIndex.data(LiveboardDepartureRole)))
     {
         // Warn
         return;
     }
+
     Liveboard::Departure tLiveboardDeparture = qVariantValue<Liveboard::Departure>(iIndex.data(LiveboardDepartureRole));
 
+
+    //
+    // Configure layout
+    //
+
+    QStyledItemDelegate::paint(iPainter, iOption, iIndex);
+
+    // Main drawing rectangle
     QRect rect = iOption.rect;
     rect.adjust(20, 8, -20, -8);
+
+    // Main font
     QFont font = iOption.font;
+
+    // Small font
+    QFont font_small = font;
+    font_small.setPointSizeF(font.pointSizeF() * 0.70);
+
+
+    //
+    // Draw text
+    //
 
     iPainter->save();
 
-    // Header
-    QString tHeader = mStations[tLiveboardDeparture.station]->name();
-    iPainter->drawText(rect, Qt::AlignTop | Qt::AlignLeft, tHeader);
+    // Hour
+    QString tHours = tLiveboardDeparture.datetime.toLocalTime().time().toString(Qt::DefaultLocaleShortDate);
+    iPainter->drawText(rect, Qt::AlignTop | Qt::AlignLeft, tHours);
+
+    // "to" text
+    iPainter->setPen(iOption.palette.mid().color());
+    iPainter->drawText(rect.adjusted(80, 0, 0, 0), Qt::AlignTop | Qt::AlignLeft, tr("to"));
+
+    // Station
+    iPainter->setPen(iOption.palette.foreground().color());
+    QString tStation = mStations[tLiveboardDeparture.station]->name();
+    iPainter->drawText(rect.adjusted(125, 0, 0, 0), Qt::AlignTop | Qt::AlignLeft, tStation);
 
     // Platform
     QString tPlatform = tr("Platform") % " " % QString::number(tLiveboardDeparture.platform);
     iPainter->drawText(rect, Qt::AlignTop | Qt::AlignRight, tPlatform);
 
-    // Hours
-    QString tHours = tLiveboardDeparture.datetime.toLocalTime().time().toString(Qt::DefaultLocaleShortDate);
-    iPainter->setPen(iOption.palette.mid().color());
-    font.setPointSizeF(font.pointSizeF() * 0.70);
-    iPainter->setFont(font);
-    iPainter->drawText(rect, Qt::AlignBottom | Qt::AlignLeft, tHours);
+    // Delay
+    if (tLiveboardDeparture.delay != 0)
+    {
+        QString tDelay = tr("%n minute(s) delay", "", tLiveboardDeparture.delay / 60);
+        iPainter->setPen(Qt::red);
+        iPainter->setFont(font_small);
+        iPainter->drawText(rect.adjusted(125, 0, 0, 0), Qt::AlignBottom | Qt::AlignLeft, tDelay);
+    }
 
     iPainter->restore();
 }
