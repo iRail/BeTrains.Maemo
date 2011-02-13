d9cc0fd8a5f12da19893f6234a47f0983f8d126c
Tim Besard <tim.besard@gmail.com>
2011-02-13 17:28:30 +0100

    Full translation.

--- maemoapplication.cpp
+++ maemoapplication.cpp
@@ -14,5 +14,10 @@ using namespace iRail;
 //
 
 MaemoApplication::MaemoApplication(int & argc, char ** argv) : Application(argc, argv, "Maemo", "1.0-beta-1")
-{
+{    
+    // Translate the user interface
+    Q_INIT_RESOURCE(i18n_maemo);
+    QTranslator tTranslator;
+    tTranslator.load(settings().value("application/language", QLocale::system().name()).toString(), ":/i18n_maemo");
+    installTranslator(&tTranslator);
 }
