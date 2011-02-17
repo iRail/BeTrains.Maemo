de77df29a28442d152e551629aac2412aa98f4e3
Yeri Tiete <yeri@tiete.be>
2011-02-17 12:33:51 +0100

    french loc. Be sure to check it with Christophe or another French speaking person > a lot is context based. hopefully I didnt break too much xml :< x

--- i18n_maemo/fr.ts
+++ i18n_maemo/fr.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../ui/dialogs/about.cpp" line="22"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Au sujet de</translation>
     </message>
     <message>
         <location filename="../ui/dialogs/about.cpp" line="30"/>
         <source>&lt;strong&gt;BeTrains for Qt&lt;/strong&gt; is part of the %1 project, and made possible by:</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;strong&gt;BeTrains pour Qt&lt;/strong&gt; fait partie du projet %1, et r&#233;alis&#233; par: </translation>
     </message>
     <message>
         <location filename="../ui/dialogs/about.cpp" line="35"/>
         <source>... and many others.</source>
-        <translation type="unfinished"></translation>
+        <translation>... et tant d'autres.</translation>
     </message>
     <message>
         <location filename="../ui/dialogs/about.cpp" line="40"/>
         <source>The BeTrains project is based upon and related with the %1 project.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le projet BeTrains est bas&#233; sur, et en rapport avec le projet %1</translation>
     </message>
     <message>
         <location filename="../ui/dialogs/about.cpp" line="42"/>
         <source>It is however NOT affiliated with the Belgian train company!</source>
-        <translation type="unfinished"></translation>
+        <translation>Il n'est cependant pas affili&#233;e avec la SNCB!</translation>
     </message>
     <message>
         <location filename="../ui/dialogs/about.cpp" line="47"/>
         <source>Be sure to check out or site, BeTrains exists for other platforms as well!</source>
-        <translation type="unfinished"></translation>
+        <translation>N'oublier pas de visiter notre site web; BeTrains existe pour d'autres plateformes aussi!</translation>
     </message>
 </context>
 <context>
@@ -40,7 +40,7 @@
         <location filename="../../alpha/ui/connectioncontroller.cpp" line="97"/>
         <location filename="../../alpha/ui/connectioncontroller.cpp" line="108"/>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>erreur inconnue</translation>
     </message>
 </context>
 <context>
@@ -49,49 +49,49 @@
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="65"/>
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="166"/>
         <source>to</source>
-        <translation type="unfinished"></translation>
+        <translation>vers</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="74"/>
         <source>Direct</source>
-        <translation type="unfinished"></translation>
+        <translation>Direct</translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="77"/>
         <source>via %n other(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>via %n autre(s)</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="96"/>
         <source>Departure at</source>
-        <translation type="unfinished"></translation>
+        <translation>D&#233;part &#224;</translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="107"/>
         <source>%n hour(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n heure(s)</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="114"/>
         <source>%n minute(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n minute(s)</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="116"/>
         <source>en route</source>
-        <translation type="unfinished"></translation>
+        <translation>en route</translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/connectiondelegate.cpp" line="177"/>
         <source>%n minute(s) delay</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n minute(s) de retard</numerusform>
         </translation>
     </message>
 </context>
@@ -100,7 +100,7 @@
     <message>
         <location filename="../ui/auxiliary/delegates/connectionpoidelegate.cpp" line="53"/>
         <source>Platform</source>
-        <translation type="unfinished"></translation>
+        <translation>voie</translation>
     </message>
 </context>
 <context>
@@ -108,12 +108,12 @@
     <message>
         <location filename="../ui/connectionviewimpl.cpp" line="111"/>
         <source>Connections</source>
-        <translation type="unfinished"></translation>
+        <translation>Liaisons</translation>
     </message>
     <message>
         <location filename="../ui/connectionviewimpl.cpp" line="134"/>
         <source>No connections to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de connections</translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="../ui/genericviewimpl.cpp" line="67"/>
         <source>Error: </source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur: </translation>
     </message>
 </context>
 <context>
@@ -130,7 +130,7 @@
         <location filename="../../alpha/ui/liveboardcontroller.cpp" line="112"/>
         <location filename="../../alpha/ui/liveboardcontroller.cpp" line="123"/>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>erreur inconnue</translation>
     </message>
 </context>
 <context>
@@ -138,18 +138,18 @@
     <message>
         <location filename="../ui/auxiliary/delegates/liveboarddeparturedelegate.cpp" line="63"/>
         <source>to</source>
-        <translation type="unfinished"></translation>
+        <translation>vers</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/liveboarddeparturedelegate.cpp" line="73"/>
         <source>Platform</source>
-        <translation type="unfinished"></translation>
+        <translation>Voie</translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/liveboarddeparturedelegate.cpp" line="84"/>
         <source>%n minute(s) delay</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n minute(s) de retard</numerusform>
         </translation>
     </message>
 </context>
@@ -158,32 +158,32 @@
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="157"/>
         <source>The liveboard seems to be empty.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucune donn&#233;e trouv&#233;e</translation>
     </message>
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="161"/>
         <source>No new items received.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucune nouvelle donn&#233;e re&#231;ue</translation>
     </message>
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="185"/>
         <source>Departures</source>
-        <translation type="unfinished"></translation>
+        <translation>D&#233;parts</translation>
     </message>
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="209"/>
         <source>No departures to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas de d&#233;parts trouv&#233;e</translation>
     </message>
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="226"/>
         <source>Station</source>
-        <translation type="unfinished"></translation>
+        <translation>Gare</translation>
     </message>
     <message>
         <location filename="../ui/liveboardviewimpl.cpp" line="281"/>
         <source>More</source>
-        <translation type="unfinished"></translation>
+        <translation>Plus</translation>
     </message>
 </context>
 <context>
@@ -191,7 +191,7 @@
     <message>
         <location filename="../../alpha/ui/maincontroller.cpp" line="172"/>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>erreur inconnue</translation>
     </message>
 </context>
 <context>
@@ -199,47 +199,47 @@
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="69"/>
         <source>No history or favorites</source>
-        <translation type="unfinished"></translation>
+        <translation>Pas d'historique ou de favoris</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="86"/>
         <source>Plan a journey</source>
-        <translation type="unfinished"></translation>
+        <translation>Planification d'un voyage</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="92"/>
         <source>View departures</source>
-        <translation type="unfinished"></translation>
+        <translation>Voir d&#233;parts</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="113"/>
         <source>&amp;Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Outils</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="114"/>
         <source>&amp;Clear history</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Supprimer l'historique</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="117"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aide</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="118"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;&#192; propos de</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="175"/>
         <source>Remove from favourites</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer des favoris</translation>
     </message>
     <message>
         <location filename="../ui/mainviewimpl.cpp" line="180"/>
         <source>Add to favourites</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajouter aux favoris</translation>
     </message>
 </context>
 <context>
@@ -247,7 +247,7 @@
     <message>
         <location filename="../../alpha/ui/requestcontroller.cpp" line="93"/>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>erreur inconnue</translation>
     </message>
 </context>
 <context>
@@ -255,32 +255,32 @@
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="59"/>
         <source>Connections from</source>
-        <translation type="unfinished"></translation>
+        <translation>Connexion de</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="59"/>
         <source>to</source>
-        <translation type="unfinished"></translation>
+        <translation>vers</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="68"/>
         <source>Arrival</source>
-        <translation type="unfinished"></translation>
+        <translation>Arriv&#233;e</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="68"/>
         <source>Departure</source>
-        <translation type="unfinished"></translation>
+        <translation>D&#233;part</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="69"/>
         <source>at</source>
-        <translation type="unfinished"></translation>
+        <translation>&#224;</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/requestdelegate.cpp" line="103"/>
         <source>Departures at</source>
-        <translation type="unfinished"></translation>
+        <translation>D&#233;part de</translation>
     </message>
 </context>
 <context>
@@ -288,52 +288,52 @@
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="52"/>
         <source>Please fill in both locations.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pri&#232;re de remplir les 2 locations</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="185"/>
         <source>New journey</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouveau voyage</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="199"/>
         <source>Origin</source>
-        <translation type="unfinished"></translation>
+        <translation>Lieu de d&#233;&#233;part</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="209"/>
         <source>Destination</source>
-        <translation type="unfinished"></translation>
+        <translation>Destination</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="234"/>
         <source>Departure</source>
-        <translation type="unfinished"></translation>
+        <translation>D&#233;part</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="235"/>
         <source>Arrival</source>
-        <translation type="unfinished"></translation>
+        <translation>Arriv&#233;e</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="246"/>
         <source>Specify time and date</source>
-        <translation type="unfinished"></translation>
+        <translation>Pr&#233;ciser date et heure</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="284"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Effacer</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="285"/>
         <source>Set to now</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliser l'heure actuelle</translation>
     </message>
     <message>
         <location filename="../ui/requestviewimpl.cpp" line="286"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Chercher</translation>
     </message>
 </context>
 <context>
@@ -341,12 +341,12 @@
     <message>
         <location filename="../ui/dialogs/stationchooser.cpp" line="70"/>
         <source>Pick a station</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisissez une gare</translation>
     </message>
     <message>
         <location filename="../ui/dialogs/stationchooser.cpp" line="92"/>
         <source>No stations found</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucune gare trouv&#233;e</translation>
     </message>
 </context>
 <context>
@@ -355,7 +355,7 @@
         <location filename="../../alpha/ui/vehiclecontroller.cpp" line="96"/>
         <location filename="../../alpha/ui/vehiclecontroller.cpp" line="107"/>
         <source>unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur inconnue</translation>
     </message>
 </context>
 <context>
@@ -363,18 +363,18 @@
     <message>
         <location filename="../ui/auxiliary/delegates/vehiclestopdelegate.cpp" line="63"/>
         <source>at</source>
-        <translation type="unfinished"></translation>
+        <translation>&#224;</translation>
     </message>
     <message>
         <location filename="../ui/auxiliary/delegates/vehiclestopdelegate.cpp" line="73"/>
         <source>Platform</source>
-        <translation type="unfinished"></translation>
+        <translation>Voie</translation>
     </message>
     <message numerus="yes">
         <location filename="../ui/auxiliary/delegates/vehiclestopdelegate.cpp" line="84"/>
         <source>%n minute(s) delay</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n minute(s) de retard</numerusform>
         </translation>
     </message>
 </context>
@@ -383,12 +383,12 @@
     <message>
         <location filename="../ui/vehicleviewimpl.cpp" line="94"/>
         <source>Connection detail</source>
-        <translation type="unfinished"></translation>
+        <translation>Info sur la liaison</translation>
     </message>
     <message>
         <location filename="../ui/vehicleviewimpl.cpp" line="116"/>
         <source>No stops to be shown.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucun arr&#234;t trouv&#233;</translation>
     </message>
 </context>
 </TS>
